3cb853d1f0a3813593b1fa32bdedc593b23f57ad
Grant <grant@Grants-MacBook-Pro.local>
2024-08-16 21:00:22 +0200

    d

--- src/components/JobDetails.js
+++ src/components/JobDetails.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Size } from "media-query";
 import Slide from "@mui/material/Slide";
 import { TextField } from "@mui/material";
 import Cookies from "universal-cookie";
@@ -8,104 +9,256 @@ const cookies = new Cookies();
 
 export const JobDetails = () => {
   Moment.locale("en");
+  const size = Size();
 
   const SiteDetails = () => {
     return (
-      <div className="grid grid-flow-row gap-1">
-        <div className="grid grid-flow-col gap-1">
-          <div className="text-center my-auto text-gray-light">
-            Site access ref no
+      <>
+        {size == "SM" || size == "XS" ? (
+          <div className="grid grid-flow-row gap-1">
+            <div className="grid grid-flow-col gap-1">
+              <div className="text-center my-auto text-gray-light">
+                Site access ref no
+              </div>
+              <div className="my-auto">
+                <TextField
+                  sx={{ backgroundColor: "white" }}
+                  className="rounded-md"
+                  size="small"
+                  fullWidth="true"
+                  placeholder="ex J432212"
+                />
+              </div>
+            </div>
+            <div className="grid grid-flow-col gap-1">
+              <div className="text-center my-auto text-gray-light">
+                GPS co-ordinates
+              </div>
+              <div className="my-auto">
+                <TextField
+                  sx={{ backgroundColor: "white" }}
+                  className="rounded-md"
+                  size="small"
+                  fullWidth="true"
+                  placeholder="ex 39.23.45.12.55"
+                />
+              </div>
+            </div>
           </div>
-          <div className="my-auto">
-            <TextField
-              sx={{ backgroundColor: "white" }}
-              className="rounded-md"
-              size="small"
-              fullWidth="true"
-              placeholder="ex J432212"
-            />
+        ) : null}
+        {size == "MD" ? (
+          <div className="grid grid-flow-row gap-1">
+            <div className="grid grid-flow-col gap-1">
+              <div className="my-auto">
+                <TextField
+                  sx={{ backgroundColor: "white" }}
+                  className="rounded-md"
+                  size="small"
+                  fullWidth="true"
+                  placeholder="Site access ref no"
+                />
+              </div>
+            </div>
+            <div className="grid grid-flow-col gap-1">
+              <div className="my-auto">
+                <TextField
+                  sx={{ backgroundColor: "white" }}
+                  className="rounded-md"
+                  size="small"
+                  fullWidth="true"
+                  placeholder="GPS co-ordinates"
+                />
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="grid grid-flow-col gap-1">
-          <div className="text-center my-auto text-gray-light">
-            GPS co-ordinates
+        ) : null}
+        {size == "L" ? (
+          <div className="grid grid-flow-row gap-1">
+            <div className="grid grid-flow-col gap-1">
+              <div className="my-auto">
+                <TextField
+                  sx={{ backgroundColor: "white" }}
+                  className="rounded-md"
+                  size="small"
+                  fullWidth="true"
+                  placeholder="Site access ref no"
+                />
+              </div>
+            </div>
+            <div className="grid grid-flow-col gap-1">
+              <div className="my-auto">
+                <TextField
+                  sx={{ backgroundColor: "white" }}
+                  className="rounded-md"
+                  size="small"
+                  fullWidth="true"
+                  placeholder="GPS co-ordinates"
+                />
+              </div>
+            </div>
           </div>
-          <div className="my-auto">
-            <TextField
-              sx={{ backgroundColor: "white" }}
-              className="rounded-md"
-              size="small"
-              fullWidth="true"
-              placeholder="ex 39.23.45.12.55"
-            />
+        ) : null}
+        {size == "XL" ? (
+          <div className="grid grid-flow-col gap-1">
+            <div className="grid grid-flow-col gap-1">
+              <div className="text-center my-auto text-gray-light">
+                Site access ref no
+              </div>
+              <div className="my-auto">
+                <TextField
+                  sx={{ backgroundColor: "white" }}
+                  className="rounded-md"
+                  size="small"
+                  fullWidth="true"
+                  placeholder="ex J432212"
+                />
+              </div>
+            </div>
+            <div className="grid grid-flow-col gap-1">
+              <div className="text-center my-auto text-gray-light">
+                GPS co-ordinates
+              </div>
+              <div className="my-auto">
+                <TextField
+                  sx={{ backgroundColor: "white" }}
+                  className="rounded-md"
+                  size="small"
+                  fullWidth="true"
+                  placeholder="ex 39.23.45.12.55"
+                />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        ) : null}
+      </>
     );
   };
 
   return (
     <Slide direction="left" in={true} timeout={800}>
-      <div
-        style={{ backgroundColor: Background1, opacity: "90%" }}
-        className="bg-gray-light rounded-md p-1 ml-1 w-full"
-      >
-        <div className="grid grid-flow-col gap-2 rounded-md p-1">
-          <div
-            style={{ backgroundColor: Background2, opacity: "90%" }}
-            className="rounded-md p-2"
-          >
-            <SiteDetails />
-          </div>
-          <div
-            style={{ backgroundColor: Background2, opacity: "90%" }}
-            className="rounded-md p-2"
-          >
-            <div className="text-center my-auto text-gray-light">Date</div>
+      {size == "XL" ? (
+        <div
+          style={{ backgroundColor: Background1, opacity: "90%" }}
+          className="bg-gray-light rounded-md p-1 ml-1 w-full"
+        >
+          <div className="grid grid-flow-col gap-2 rounded-md p-1">
             <div
-              style={{
-                minHeight: "30pt",
-                alignContent: "center",
-              }}
-              className="text-center text-gray-light"
+              style={{ backgroundColor: Background2, opacity: "90%" }}
+              className="rounded-md p-2"
             >
-              {Moment(Date()).format("MMMM Do YYYY, h:mm a")}
-            </div>
-          </div>
-          <div
-            style={{ backgroundColor: Background2, opacity: "90%" }}
-            className="rounded-md p-2"
-          >
-            <div className="text-center my-auto text-gray-light">
-              Technician
+              <SiteDetails />
             </div>
             <div
-              style={{
-                minHeight: "30pt",
-                alignContent: "center",
-              }}
-              className="text-center text-gray-light"
+              style={{ backgroundColor: Background2, opacity: "90%" }}
+              className="inline-flex rounded-md p-2"
             >
-              {cookies.get("Username")}
+              <div className="text-center my-auto text-gray-light mr-2">Date:</div>
+              <div
+                style={{
+                  minHeight: "30pt",
+                  alignContent: "center",
+                }}
+                className="text-center text-gray-light"
+              >
+                {Moment(Date()).format("MMMM Do YYYY, h:mm a")}
+              </div>
             </div>
-          </div>
-          <div
-            style={{ backgroundColor: Background2, opacity: "90%" }}
-            className="rounded-md p-2"
-          >
-            <div className="text-center my-auto text-gray-light">Site</div>
-            <div className="flex align-center justify-center">
-              <TextField
-                sx={{ backgroundColor: "white" }}
-                className="rounded-md"
-                size="small"
-                fullWidth="true"
-                placeholder="ex Kannonkop"
-              />
+            <div
+              style={{ backgroundColor: Background2, opacity: "90%" }}
+              className="inline-flex rounded-md p-2"
+            >
+              <div className="text-center my-auto text-gray-light">
+                Technician
+              </div>
+              <div
+                style={{
+                  minHeight: "30pt",
+                  alignContent: "center",
+                }}
+                className="text-center text-gray-light"
+              >
+                {cookies.get("Username")}
+              </div>
+            </div>
+            <div
+              style={{ backgroundColor: Background2, opacity: "90%" }}
+              className="inline-flex rounded-md p-2"
+            >
+              <div className="text-center my-auto text-gray-light mr-2">Site:</div>
+              <div className="flex align-center justify-center">
+                <TextField
+                  sx={{ backgroundColor: "white" }}
+                  className="rounded-md"
+                  size="small"
+                  fullWidth="true"
+                  placeholder="ex Kannonkop"
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div
+          style={{ backgroundColor: Background1, opacity: "90%" }}
+          className="bg-gray-light rounded-md p-1 ml-1 w-full"
+        >
+          <div className="grid grid-flow-col gap-2 rounded-md p-1">
+            <div
+              style={{ backgroundColor: Background2, opacity: "90%" }}
+              className="rounded-md p-2"
+            >
+              <SiteDetails />
+            </div>
+            <div
+              style={{ backgroundColor: Background2, opacity: "90%" }}
+              className="rounded-md p-2"
+            >
+              <div className="text-center my-auto text-gray-light">Date</div>
+              <div
+                style={{
+                  minHeight: "30pt",
+                  alignContent: "center",
+                }}
+                className="text-center text-gray-light"
+              >
+                {Moment(Date()).format("MMMM Do YYYY, h:mm a")}
+              </div>
+            </div>
+            <div
+              style={{ backgroundColor: Background2, opacity: "90%" }}
+              className="rounded-md p-2"
+            >
+              <div className="text-center my-auto text-gray-light">
+                Technician
+              </div>
+              <div
+                style={{
+                  minHeight: "30pt",
+                  alignContent: "center",
+                }}
+                className="text-center text-gray-light"
+              >
+                {cookies.get("Username")}
+              </div>
+            </div>
+            <div
+              style={{ backgroundColor: Background2, opacity: "90%" }}
+              className="rounded-md p-2"
+            >
+              <div className="text-center my-auto text-gray-light">Site</div>
+              <div className="flex align-center justify-center">
+                <TextField
+                  sx={{ backgroundColor: "white" }}
+                  className="rounded-md"
+                  size="small"
+                  fullWidth="true"
+                  placeholder="ex Kannonkop"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </Slide>
   );
 };
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Size } from "media-query";
 import Button from "@mui/material/Button";
 import { TextField } from "@mui/material";
 import Switch from "@mui/material/Switch";
@@ -10,76 +11,149 @@ import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+
 
 export const SearchBar = () => {
+  const size = Size();
   return (
     <Slide direction="left" in={true} timeout={1200}>
-      <div
-        style={{ backgroundColor: Background1, opacity: "90%" }}
-        className="rounded-md p-2 ml-1"
-      >
-        <div className="grid grid-cols-3 gap-2 rounded-md">
-          <div
-            style={{ backgroundColor: Background2, opacity: "90%" }}
-            className="rounded-md p-4 shadow-md"
-          >
-            <div className="my-auto mx-auto">
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DemoContainer components={["DatePicker"]}>
-                  <DatePicker  closeOnSelect={true} />
-                </DemoContainer>
-              </LocalizationProvider>
-            </div>
-          </div>
-          <div
-            style={{ backgroundColor: Background2, opacity: "90%" }}
-            className="rounded-md p-1 shadow-inner"
-          >
-            <div className="rounded-md text-center my-auto text-gray-light mb-1">
-              Search by
-            </div>
-            <div className="rounded-md grid grid-cols-3 p-1">
-              <div className="text-center my-auto text-gray-light">Site</div>
-              <div className="flex align-center justify-center">
-                <Switch color="error" />
-              </div>
-              <div className="text-center my-auto text-gray-light">
-                Technician
+      {size == "SM" || size == "XS" || size == "MD" ? (
+        <div
+          style={{ backgroundColor: Background1, opacity: "90%" }}
+          className="rounded-md p-2 ml-1"
+        >
+          <div className="grid grid-cols-3 gap-2 rounded-md">
+            <div
+              style={{ backgroundColor: Background2, opacity: "90%" }}
+              className="rounded-md p-4 shadow-md"
+            >
+              <div className="my-auto mx-auto">
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DemoContainer components={["MobileDatePicker"]}>
+                    <MobileDatePicker closeOnSelect={true} />
+                  </DemoContainer>
+                </LocalizationProvider>
               </div>
             </div>
-          </div>
-          <div
-            style={{ backgroundColor: Background2, opacity: "90%" }}
-            className="grid grid-cols-2 rounded-md p-1 shadow-md"
-          >
-            <div className="ml-2 my-auto">
-              <TextField
-                sx={{ backgroundColor: "white" }}
-                className="rounded-md"
-                size="small"
-                fullWidth="true"
-              />
-            </div>
-            <div className="flex mx-auto my-auto">
-              <Button
-                size="small"
-                fullwidth="true"
-                className="shadow-md"
-                sx={{
-                  backgroundColor: "#e3e3e3",
-                  opacity: "75%",
-                  maxHeight: "30pt",
-                }}
-              >
-                <div className="p-2 text-gray-dark">
-                  Search
-                  <SearchIcon />
+            <div
+              style={{ backgroundColor: Background2, opacity: "90%" }}
+              className="rounded-md p-1 shadow-inner"
+            >
+              <div className="rounded-md text-center my-auto text-gray-light mb-1">
+                Search by
+              </div>
+              <div className="rounded-md grid grid-cols-3 p-1">
+                <div className="text-center my-auto text-gray-light">Site</div>
+                <div className="flex align-center justify-center">
+                  <Switch color="error" />
                 </div>
-              </Button>
+                <div className="text-center my-auto text-gray-light">
+                  Technician
+                </div>
+              </div>
+            </div>
+            <div
+              style={{ backgroundColor: Background2, opacity: "90%" }}
+              className="grid grid-flow-row rounded-md p-1 shadow-md"
+            >
+              <div className="ml-2 my-auto">
+                <TextField
+                  sx={{ backgroundColor: "white" }}
+                  className="rounded-md"
+                  size="small"
+                  fullWidth="true"
+                />
+              </div>
+              <div className="flex mx-auto my-auto">
+                <Button
+                  size="small"
+                  fullwidth="true"
+                  className="shadow-md"
+                  sx={{
+                    backgroundColor: "#e3e3e3",
+                    opacity: "75%",
+                    maxHeight: "30pt",
+                  }}
+                >
+                  <div className="p-2 text-gray-dark">
+                    Search
+                    <SearchIcon />
+                  </div>
+                </Button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div
+          style={{ backgroundColor: Background1, opacity: "90%" }}
+          className="rounded-md p-2 ml-1"
+        >
+          <div className="grid grid-cols-3 gap-2 rounded-md">
+            <div
+              style={{ backgroundColor: Background2, opacity: "90%" }}
+              className="rounded-md p-4 shadow-md"
+            >
+              <div className="my-auto mx-auto">
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DemoContainer components={["DatePicker"]}>
+                    <DatePicker size="small" orientation="portrait" closeOnSelect={true} />
+                  </DemoContainer>
+                </LocalizationProvider>
+              </div>
+            </div>
+            <div
+              style={{ backgroundColor: Background2, opacity: "90%" }}
+              className="rounded-md p-1 shadow-inner"
+            >
+              <div className="rounded-md text-center my-auto text-gray-light mb-1">
+                Search by
+              </div>
+              <div className="rounded-md grid grid-cols-3 p-1">
+                <div className="text-center my-auto text-gray-light">Site</div>
+                <div className="flex align-center justify-center">
+                  <Switch color="error" />
+                </div>
+                <div className="text-center my-auto text-gray-light">
+                  Technician
+                </div>
+              </div>
+            </div>
+            <div
+              style={{ backgroundColor: Background2, opacity: "90%" }}
+              className="grid grid-flow-row rounded-md p-1 shadow-md"
+            >
+              <div className="my-auto">
+                <TextField
+                  sx={{ backgroundColor: "white" }}
+                  className="rounded-md"
+                  size="small"
+                  fullWidth="true"
+                />
+              </div>
+              <div className="flex mx-auto my-auto">
+                <Button
+                  size="small"
+                  fullwidth="true"
+                  className="shadow-md"
+                  sx={{
+                    backgroundColor: "#e3e3e3",
+                    opacity: "75%",
+                    maxHeight: "30pt",
+                  }}
+                >
+                  <div className="p-2 text-gray-dark">
+                    Search
+                    <SearchIcon />
+                  </div>
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </Slide>
   );
 };
