9f97ea21659f78d68fd28fd78a8df0995a7479f6
Grant <grant@Grants-MacBook-Pro.local>
2024-08-19 16:22:48 +0200

    d

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -2,17 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Size } from "media-query";
 import Button from "@mui/material/Button";
 import { TextField } from "@mui/material";
-import Switch from "@mui/material/Switch";
 import "react-datepicker/dist/react-datepicker.css";
 import Slide from "@mui/material/Slide";
 import SearchIcon from "@mui/icons-material/Search";
 import { Background1, Background2, SaveButtonStyle } from "./Display/feutures";
-import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
 import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { MenuSimple } from "components/DropDown";
 
 const SearchBy = () => {
   const size = Size();
@@ -21,51 +18,24 @@ const SearchBy = () => {
       {size == "L" ? (
         <div
           style={{ backgroundColor: Background2, opacity: "90%" }}
-          className="rounded-md p-1 shadow-inner"
+          className="flex justify-center rounded-md p-1 shadow-inner"
         >
-          <div className="rounded-md text-center my-auto text-gray-light">
-            Search by
-          </div>
-          <div className="rounded-md grid grid-rows-2 gap-1 p-1">
-            <Button
-              size="small"
-              sx={{
-                backgroundColor: "#e3e3e3",
-                opacity: "75%",
-                maxHeight: "30pt",
-              }}
-            >
-              <div className="text-center my-auto text-gray-dark">Site</div>
-            </Button>
-            <Button
-              size="small"
-              sx={{
-                backgroundColor: "#e3e3e3",
-                opacity: "75%",
-                maxHeight: "30pt",
-              }}
-            >
-              <div className="text-center my-auto text-gray-dark">
-                Technician
-              </div>
-            </Button>
+          <div className="block">
+            <div className="text-center my-auto text-gray-light">Search By</div>
+            <div className="my-auto">
+              <MenuSimple />
+            </div>
           </div>
         </div>
       ) : (
         <div
           style={{ backgroundColor: Background2, opacity: "90%" }}
-          className="rounded-md p-1 shadow-inner"
+          className="flex justify-center rounded-md p-1 shadow-inner"
         >
-          <div className="rounded-md text-center my-auto text-gray-light">
-            Search by
-          </div>
-          <div className="rounded-md grid grid-cols-3 p-1">
-            <div className="text-center my-auto text-gray-light">Site</div>
-            <div className="flex align-center justify-center">
-              <Switch color="error" />
-            </div>
-            <div className="text-center my-auto text-gray-light">
-              Technician
+          <div className="block">
+            <div className="text-center my-auto text-gray-light">Search By</div>
+            <div className="my-auto">
+              <MenuSimple />
             </div>
           </div>
         </div>
@@ -164,7 +134,7 @@ export const SearchBar = () => {
           style={{ backgroundColor: Background1, opacity: "90%" }}
           className="rounded-md p-2 mt-2"
         >
-          <div className="grid grid-flow-row gap-2 rounded-md">
+          <div className="grid grid-rows-3 gap-2 rounded-md">
             <div
               style={{ backgroundColor: Background2, opacity: "90%" }}
               className="flex align-center justify-center rounded-md p-4 shadow-md"
@@ -180,7 +150,7 @@ export const SearchBar = () => {
           style={{ backgroundColor: Background1, opacity: "90%" }}
           className="rounded-md p-2"
         >
-          <div className="grid grid-flow-col gap-2 rounded-md">
+          <div className="grid grid-cols-3 gap-2 rounded-md">
             <div
               style={{ backgroundColor: Background2, opacity: "90%" }}
               className="flex align-center justify-center rounded-md p-4 shadow-md"
